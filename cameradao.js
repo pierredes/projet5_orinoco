f8774e41086acf2d5bb81642379161fce3d52373
pierredes <pierredesombre.pro@gmail.com>
2020-09-14 16:10:16 +0200

    add statit method and export class

--- cameradao.js
+++ cameradao.js
@@ -1,4 +1,4 @@
-class CameraDAO {
+export class CameraDAO {
     
     static getAllCamera() {
         return fetch('http://localhost:3000/api/cameras').then(res => {
@@ -11,7 +11,7 @@ class CameraDAO {
         });
     }
 
-    getCameraById(id){
+    static getCameraById(id){
         return fetch('http://localhost:3000/api/cameras/' + id).then(res => {
             if(res.ok) {
                 var donnees = res.json();
