128f1ba315ab9f74ce40e601ad8808b05cb0b5ef
pierredes <pierredesombre.pro@gmail.com>
2020-09-15 15:32:52 +0200

    add search method using url and a get data method from this url

--- camera.js
+++ camera.js
@@ -1,18 +1,19 @@
 import {CameraDAO} from './cameradao.js';
 export class Camera {
-    constructor(id, nom, prix, description, image) {
+    constructor(id, nom, prix, description, image, personnalisation) {
         this.id = id;
         this.nom = nom;
         this.prix = prix;
         this.description = description;
         this.image = image;
+        this.personnalisation = personnalisation;
     }
 
     findAllCamera() {
         let camera = [];
         return CameraDAO.getAllCamera().then(data => {
             for(let i=0 ; i<data.length ; i++){
-                camera[i] = new Camera (data[i]._id, data[i].name, data[i].price, data[i].description, data[i].imageUrl);
+                camera[i] = new Camera (data[i]._id, data[i].name, data[i].price, data[i].description, data[i].imageUrl, data[i].lenses);
             }
             return camera;
         });
@@ -46,5 +47,21 @@ export class Camera {
             }
             return cameraImage;
         });       
+    }
+
+    findOneCameraByUrlId() {
+        let url = new URL(document.location);
+        let parametre = url.searchParams;
+        let id = parametre.get('id');
+        return id;
+    }
+
+    findOneCamera() {
+        let id = this.findOneCameraByUrlId();
+        let camera;
+        return CameraDAO.getCameraById(id).then(data => {
+            camera = new Camera(data._id, data.name, data.price, data.description, data.imageUrl, data.lenses)
+            return camera;  
+        })
     }
 }
