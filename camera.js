77cee2f39cb7d18e0f9613ca1b3c59d78cd52d97
pierredes <pierredesombre.pro@gmail.com>
2020-09-17 07:58:47 +0200

    remove all camera method (for add them in dao)

--- camera.js
+++ camera.js
@@ -1,4 +1,3 @@
-import {CameraDAO} from './cameradao.js';
 export class Camera {
     constructor(id, nom, prix, description, image, personnalisation) {
         this.id = id;
@@ -9,59 +8,4 @@ export class Camera {
         this.personnalisation = personnalisation;
     }
 
-    findAllCamera() {
-        let camera = [];
-        return CameraDAO.getAllCamera().then(data => {
-            for(let i=0 ; i<data.length ; i++){
-                camera[i] = new Camera (data[i]._id, data[i].name, data[i].price, data[i].description, data[i].imageUrl, data[i].lenses);
-            }
-            return camera;
-        });
-    }
-
-    findAllCameraByName() {
-        let cameraName = [];
-        return CameraDAO.getAllCamera().then(data => {
-            for(let i=0 ; i<data.length ; i++){
-                cameraName.push(data[i].name);
-            }
-            return cameraName;
-        });       
-    }
-
-    findAllCameraByPrice() {
-        let cameraPrix = [];
-        return CameraDAO.getAllCamera().then(data => {
-            for(let i=0 ; i<data.length ; i++){
-                cameraPrix.push(data[i].price);
-            }
-            return cameraPrix;
-        });       
-    }
-
-    findAllCameraByImage() {
-        let cameraImage = [];
-        return CameraDAO.getAllCamera().then(data => {
-            for(let i=0 ; i<data.length ; i++){
-                cameraImage.push(data[i].imageUrl);
-            }
-            return cameraImage;
-        });       
-    }
-
-    findOneCameraByUrlId() {
-        let url = new URL(document.location);
-        let parametre = url.searchParams;
-        let id = parametre.get('id');
-        return id;
-    }
-
-    findOneCamera() {
-        let id = this.findOneCameraByUrlId();
-        let camera;
-        return CameraDAO.getCameraById(id).then(data => {
-            camera = new Camera(data._id, data.name, data.price, data.description, data.imageUrl, data.lenses)
-            return camera;  
-        })
-    }
 }
