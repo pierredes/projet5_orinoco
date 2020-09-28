eaf99f46c7458a2215fc1251493cddf3ebeea957
pierredes <pierredesombre.pro@gmail.com>
2020-09-28 12:56:55 +0200

    add get url method

--- produit.js
+++ produit.js
@@ -1,46 +1,47 @@
 import {CameraDAO} from './cameradao.js';
+import {Commande} from './commande.js';
 $(document).ready( () => {
    
-
-
     let article_photo = document.getElementById('article_photo');
     let nom_camera = document.getElementById('nom_camera');
     let description_camera = document.getElementById('description_camera');
     let prix_camera = document.getElementById('prix_camera');
     let option_personnalisation = document.getElementById('option_personnalisation');
 
-    CameraDAO.findOneCamera().then(data => {
+    // Récupération de l'id
+    let url = new URL(document.location);
+    let parametre = url.searchParams;
+    let id = parametre.get('id');
+
+
+    // Affchage des données de la caméra
+    CameraDAO.findOneCamera(id).then(data => {
         article_photo.innerHTML = ('<img src=' + data.image + '>');
         nom_camera.innerHTML = data.nom;
         description_camera.innerHTML = data.description;
-        prix_camera.innerHTML = data.prix;
+        prix_camera.innerHTML = 'Prix : ' + data.prix + ' EUROS';
         let tableau_personnalisation = data.personnalisation;
         for(let i = 0; i < tableau_personnalisation.length; i++){
             option_personnalisation.innerHTML += ('<option>' + tableau_personnalisation[i] + '</option>');
         }
     });
 
+    // Redirection vers la page panier
     let voir_panier = document.getElementById('voir_panier');
     voir_panier.addEventListener('click', () => {
         window.location = "panier.html";
     });
 
+    // Redirection vers la page index
     let retour_accueil = document.getElementById('retour_accueil');
     retour_accueil.addEventListener('click', () => {
         window.location = 'index.html';
     });
 
+    // Ajout de la caméra dans le localstorage
     let ajouter_panier = document.getElementById('ajouter_panier');
     ajouter_panier.addEventListener('click', () => {
-        if(localStorage.getItem('camera') == null){
-            var contenu_panier = [];
-        }else{
-            contenu_panier =  JSON.parse(localStorage.getItem('camera'));
-        }
-        CameraDAO.findOneCamera().then(data => {
-            contenu_panier.push(data);
-            localStorage.setItem('camera', JSON.stringify(contenu_panier));
-        });
+        Commande.addItemInCommande('camera');
     });
     
 });
