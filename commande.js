0e7c7e4a3a9eb41e351511925a2d34262625bd24
pierredes <pierredesombre.pro@gmail.com>
2020-09-28 12:48:23 +0200

    Create a class commande with get and add method

---
+++ commande.js
@@ -0,0 +1,24 @@
+import {CameraDAO} from './cameradao.js';
+export class Commande {
+
+    // Ajouter une caméra dans le panier
+    static addItemInCommande(item) {
+        if(localStorage.getItem(item) == null){
+            var contenu_panier = [];
+        }
+        else{
+            contenu_panier = JSON.parse(localStorage.getItem(item));
+        }
+            
+        CameraDAO.findOneCamera().then(data => {
+            contenu_panier.push(data);
+            localStorage.setItem(item, JSON.stringify(contenu_panier));
+        });
+    }
+
+    // Récupère toute les caméras du localstorage
+    static GetAllItemFromCommande(item) {
+        let getcommande = JSON.parse(localStorage.getItem(item));
+        return getcommande;
+    }
+}
