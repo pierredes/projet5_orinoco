5c01133c5c44895400f75c5207d2e6d0500ead0b
pierredes <pierredesombre.pro@gmail.com>
2020-09-29 16:01:48 +0200

    change project organization and path

--- contact.js
+++
@@ -1,9 +0,0 @@
-export class Contact {
-    constructor(prenom, nom, adresse, ville, email) {
-        this.firstName = prenom;
-        this.lastName = nom;
-        this.address = adresse;
-        this.city = ville;
-        this.email = email;
-    }
-}
