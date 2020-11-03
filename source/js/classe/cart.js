7c8bf0dc820a4e5606a080219b6d7381698b09cf
pierredes <pierredesombre.pro@gmail.com>
2020-11-03 17:49:10 +0100

    change add and get method in cart

--- source/js/classe/cart.js
+++ source/js/classe/cart.js
@@ -2,44 +2,27 @@ export class Cart {
 
     // Ajouter une caméra dans le panier
     static addItemInCart(item) {
-        if(localStorage.getItem(item.id) == null){
-            var contenu_panier = [];
+        if(localStorage.getItem(item.id) === null) {
+            item.quantite = 1;
+            localStorage.setItem(item.id, JSON.stringify(item));
         }
-        else{
-            contenu_panier = JSON.parse(localStorage.getItem(item.id));
-        }  
-            contenu_panier.push(item);
-            localStorage.setItem(item.id, JSON.stringify(contenu_panier));
+        else {
+            let camera = JSON.parse(localStorage.getItem(item.id));
+            camera.quantite++;
+            localStorage.setItem(item.id, JSON.stringify(camera));
+        }
     }
 
     // Récupère toute les caméras du localstorage
-    static listItemFromCart(item) {
-        let itemInCart = JSON.parse(localStorage.getItem(item));
-        return itemInCart;
-    }
-
-     // Supprime une camera du panier
-     static removeItemFromCart(id) {
-        let allItemInCart = this.listItemFromCart('camera');
-        let name = 'Zurss 50S';
-        for(let i = 0; i < allItemInCart.length; i++){
-            if(allItemInCart[i].id == id) {
-                localStorage.removeItem('camera' + i);
+    static listItemFromCart() {
+        let itemInCart = [];
+        let cleAIgnorer = 'quantite :';
+        for(let i=0; i<localStorage.length; i++) {
+            let key = localStorage.key(i);
+            if(key.search(cleAIgnorer) != 0) {
+                itemInCart.push(JSON.parse(localStorage.getItem(key)));
             }
         }
+        return itemInCart;
     }
 }
-
-
-
-
-//static addItemInCart(item) {
-    //     if(localStorage.getItem('camera') == null){
-    //         var contenu_panier = [];
-    //     }
-    //     else{
-    //         contenu_panier = JSON.parse(localStorage.getItem('camera'));
-    //     } 
-    //     contenu_panier.push(item);
-    //     localStorage.setItem('camera', JSON.stringify(contenu_panier));
-    // }
